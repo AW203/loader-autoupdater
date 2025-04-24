3b7fd0e089781536cc2770c7a277d999fb0b0fd3
AW203 <avril27wy@lycee-ndduroc.com>
2025-04-24 23:28:26 +0200

    Translate compile scripts to English and fix file naming

--- compile.bat
+++ compile.bat
@@ -1,22 +1,20 @@
 @echo off
 cd C:\Users\Yayam\Music\loader-autoupdate
-echo Compilation du Auto-Update Loader (Version 0.9.0)...
+echo Compiling Auto-Update Loader (Version 0.9.0)...
 
-echo Compilation des ressources...
+echo Compiling resources...
 windres loader_version.rc -O coff -o loader_version.res
 
-echo Compilation du programme...
-g++ simple_loader.cpp loader_version.res -o loader.exe -lwininet -lurlmon -lshell32
+echo Compiling program...
+g++ simple_loader.cpp loader_version.res -o loader_0.9.0.exe -lwininet -lurlmon -lshell32
 
 if %ERRORLEVEL% == 0 (
-    echo Compilation réussie!
-    echo Création d'une copie nommée loader_0.9.0.exe...
-    copy loader.exe loader_0.9.0.exe
-    echo L'exécutable loader.exe (et loader_0.9.0.exe) a été créé.
-    echo Pour tester le programme, lancez loader_0.9.0.exe
+    echo Compilation successful!
+    echo The executable loader_0.9.0.exe has been created.
+    echo To test the program, run loader_0.9.0.exe
 ) else (
-    echo Erreur de compilation!
-    echo Vérifiez que TDM-GCC est correctement installé et que tous les chemins sont configurés.
+    echo Compilation error!
+    echo Make sure TDM-GCC is properly installed and all paths are configured.
 )
 
 pause 
--- compile_release.bat
+++ compile_release.bat
@@ -1,19 +1,20 @@
 @echo off
-echo Compilation du Auto-Update Loader (Version Release 1.0.0)...
+cd C:\Users\Yayam\Music\loader-autoupdate
+echo Compiling Auto-Update Loader (Release Version 1.0.0)...
 
-echo Compilation des ressources...
+echo Compiling resources...
 windres release_version.rc -O coff -o release_version.res
 
-echo Compilation du programme...
-g++ release_loader.cpp release_version.res -o loader.exe -lwininet -lurlmon -lshell32
+echo Compiling program...
+g++ release_loader.cpp release_version.res -o loader_1.0.0.exe -lwininet -lurlmon -lshell32
 
 if %ERRORLEVEL% == 0 (
-    echo Compilation réussie!
-    echo L'exécutable loader.exe (version 1.0.0) a été créé.
-    echo Ce fichier doit être téléversé dans la release GitHub.
+    echo Compilation successful!
+    echo The executable loader_1.0.0.exe has been created.
+    echo This file should be uploaded to GitHub releases.
 ) else (
-    echo Erreur de compilation!
-    echo Vérifiez que TDM-GCC est correctement installé et que tous les chemins sont configurés.
+    echo Compilation error!
+    echo Make sure TDM-GCC is properly installed and all paths are configured.
 )
 
 pause 
